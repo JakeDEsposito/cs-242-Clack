dee66595e88ceb1c662828188d8d252fe7f62262
Crazy_Human <zarriegp@clarkson.edu>
2022-11-30 09:27:28 -0500

    Methods for Pt4.2.1 implemented needs stuff from Pt4.2.2 to be able to compile and run

--- src/main/ServerSideClientIO.java
+++ src/main/ServerSideClientIO.java
@@ -2,6 +2,7 @@ package main;
 
 import data.ClackData;
 
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
@@ -15,19 +16,43 @@ public class ServerSideClientIO implements Runnable{
     private ClackServer server;
     private Socket clientSocket;
 
-    public ServerSideClientIO(ClackServer s, Socket c){
-        server=s;
-        clientSocket=c;
+    public ServerSideClientIO(ClackServer server, Socket clientSocket){
+        this.server=server;
+        this.clientSocket=clientSocket;
+        this.closeConnection=false;
     }
 
     @Override
     public void run(){
+        try {
+            outToClient = new ObjectOutputStream(clientSocket.getOutputStream());
+            inFromClient = new ObjectInputStream(clientSocket.getInputStream());
+
+            while(!closeConnection){
+                receiveData();
+                this.server.broadcast();
+            }
+        } catch(IOException ioe) {
+            System.err.println(ioe);
+        }
     }
 
     public void receiveData(){
+        try {
+            dataToReceiveFromClient = (ClackData)inFromClient.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            System.err.println(e);
+            closeConnection = true;
+        }
     }
 
     public void sendData(){
+        try {
+            outToClient.writeObject(dataToSendToClient);
+        } catch (IOException ioe) {
+            System.err.println(ioe);
+            closeConnection = true;
+        }
     }
 
     public void setDataToSendToClient(ClackData dataToSendToClient) {
